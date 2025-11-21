6d0b37209c7f2f725b8db4aeca33da87c26cf2c3
Deepika Selvaraj <deepika.selvaraj@icodexsolutions.local>
2025-11-21 15:00:27 +0530

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { SkillComponent } from './pages/skill/skill.component';
+import { ProjectComponent } from './pages/project/project.component';
+import { InternshipComponent } from './pages/internship/internship.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'skills', component: SkillComponent },
+  { path: 'projects', component: ProjectComponent },
+  { path: 'internship', component: InternshipComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SkillComponent } from './pages/skill/skill.component';
+import { ProjectComponent } from './pages/project/project.component';
+import { InternshipComponent } from './pages/internship/internship.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    SkillComponent,
+    ProjectComponent,
+    InternshipComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
